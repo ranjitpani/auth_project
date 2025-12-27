2cc512853af847ecde1e37bf966640a365825b15
Ranjit Pani <paniranjit161@gmail.com>
2025-12-27 13:47:23 +0530

    Added auto-create admin and fixed settings

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        import os
+        if os.environ.get('CREATE_ADMIN', 'False') == 'true':
+            User = get_user_model()
+            admin_email = os.environ.get('ADMIN_EMAIL')
+            admin_password = os.environ.get('ADMIN_PASSWORD')
+            if admin_email and admin_password:
+                if not User.objects.filter(email=admin_email).exists():
+                    User.objects.create_superuser(
+                        email=admin_email,
+                        password=admin_password
+                    )
--- auth_project/settings.py
+++ auth_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
