fdee3cd89d92e114eab2ddaddc63a5b8330e3507
Ranjit Pani <paniranjit161@gmail.com>
2026-01-08 18:38:51 +0530

    Fix cart history html page

--- accounts/views.py
+++ accounts/views.py
@@ -1539,7 +1539,7 @@ def cancel_order(request, order_id):
 
 from django.shortcuts import render, get_object_or_404
 from .models import Order
-
+@login_required
 def order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id, user=request.user)
     return render(request, 'order_detail.html', {'order': order})
